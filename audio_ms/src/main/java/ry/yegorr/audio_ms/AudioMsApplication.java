3a563f8e884b3eaa7035cb58ab29ba667ea449d1
Egor Ryazantsev <ryazanzevev@ssp-soft.com>
2021-12-13 21:51:32 +0700

    Создал проект для второго микросервиса

---
+++ audio_ms/src/main/java/ry/yegorr/audio_ms/AudioMsApplication.java
@@ -0,0 +1,13 @@
+package ry.yegorr.audio_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioMsApplication.class, args);
+	}
+
+}
---
+++ audio_ms/src/test/java/ry/yegorr/audio_ms/AudioMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ry.yegorr.audio_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
